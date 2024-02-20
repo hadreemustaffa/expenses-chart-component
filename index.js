02ce6f7550cd942a13cfbd45097ad58c3407dfdd
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-02-20 17:14:04 +0800

    add bar chart from json data

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const chartContainers = document.querySelectorAll('.chart__bar-container');
+const total = document.querySelectorAll('.chart__bar-total');
+const day = document.querySelectorAll('.chart__bar-day');
+const daysBlock = document.querySelectorAll('.chart__bar-block');
+const totalPerDay = document.querySelectorAll('.chart__bar-total');
 
-let totalSpendings = 0;
-fetch('data.json')
-  .then((response) => response.json())
-  .then((data) => getData(data))
-  .catch((error) => console.log('Error:', error));
+let amountArr = [];
+let totalPerWeek = 0;
 
-function getData(data) {
-  chartContainers.forEach((container, index) => {
-    const day = container.querySelector('.chart__bar-day');
-    const daysBlock = container.querySelector('.chart__bar-block');
-    const totalPerDay = container.querySelector('.chart__bar-total');
+async function getData() {
+  const response = await fetch('data.json');
+  const data = await response.json();
+  return data;
+}
 
-    day.innerHTML = data[index].day;
-    totalPerDay.innerHTML = '$' + data[index].amount;
-
-    totalSpendings += data[index].amount;
+getData().then((data) => {
+  data.map((data, i) => {
+    amountArr.push(data.amount);
+    day[i].innerHTML = data.day;
+    totalPerDay[i].innerHTML = `$${data.amount}`;
   });
 
-  console.log(totalSpendings);
-}
+  const maxAmount = `${Math.max(...amountArr)}`;
+
+  totalPerDay.forEach((day) => {
+    if (day.innerHTML == `$${maxAmount}`) {
+      day.parentElement.classList.add('max');
+    }
+  });
+
+  daysBlock.forEach((block, i) => {
+    let percentage;
+    percentage = `${(amountArr[i] / maxAmount) * 100}%`;
+    block.style.height = `${percentage}`;
+  });
+});
